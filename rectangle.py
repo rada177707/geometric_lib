6cb2ddd22056e4e2ac43a92133c1b13b46bc2942
rada177707 <muhamatdinovaradmila9@gmail.com>
2026-04-28 17:25:42 +0000

    Added triangle.py and fixed rectangle perimeter bug

--- rectangle.py
+++ rectangle.py
@@ -1,4 +1,4 @@
-def area(a, b):
- return a * b
-def perimeter(a, b):
- return a + b
+    def area(a, b):
+        return a * b
+    def perimeter(a, b):
+        return 2 * (a + b)   # ← исправлено: периметр = 2*(a+b)
